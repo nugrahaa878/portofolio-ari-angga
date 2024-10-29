5e62294cebc3a2b79392468048e996fb2fa165ba
Ayunissa Rahma Yani <ari.nugraha@Aris-MacBook-Pro.local>
2024-10-29 09:22:02 +0700

    remove unsued variable

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,6 @@
 import Button from "../Button";
 
 const Navbar = () => {
-  const linkedInUrl = "https://www.linkedin.com/in/nugrahaa878/";
-  const cvLink =
-    "https://drive.google.com/file/d/19uxkHiwbusjfcXJANYVxX0KLdGCKp1iY/view?usp=sharing";
-
-  const handleRedirectLink = (url: string) => {
-    window.open(url);
-  };
 
   return (
     <div className="flex justify-between w-full pt-10 pr-24 align-middle">
