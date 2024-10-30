5d52f54ff7ef3843e301de210644c4502df257cf
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2024-10-30 11:46:23 +0700

    center footer

--- src/routes/HomePage/components/Footer/index.tsx
+++ src/routes/HomePage/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<div className="w-full bg-gray-300 py-3 px-48">
+		<div className="w-full bg-gray-300 py-3 px-48 flex items-center justify-center">
 			<p>© 2024 Ari and Angga. All Rights Reserved.</p>
 		</div>
 	)
